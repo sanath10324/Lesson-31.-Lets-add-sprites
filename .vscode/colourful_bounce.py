ccf8ab5550b895c436a9ea4944e12d5d7fa18424
sanath10324 <sanathkatkamgamer@gmail.com>
2026-07-03 10:39:08 +0400

    Colorful Bounce

--- .vscode/colourful_bounce.py
+++ .vscode/colourful_bounce.py
@@ -18,7 +18,7 @@ WHITE = pygame.Color("white")
 class Sprite(pygame.sprite.Sprite):
 
     def __init__ (self, color, height, width):
-        super.__init__()
+        super().__init__()
 
         self.image = pygame.Surface([width, height])
         self.image.fill(color)
@@ -48,3 +48,35 @@ class Sprite(pygame.sprite.Sprite):
 def change_background_color():
     global bg_color 
     bg_color = random.choice([BLUE, LIGHTBLUE, DARKBLUE])
+
+all_sprites_list = pygame.sprite.Group()
+sp1 = Sprite(WHITE, 20, 30)
+sp1.rect.x = random.randint(0, 480)
+sp1.rect.y = random.randint(0, 370)
+
+all_sprites_list.add(sp1)
+
+screen = pygame.display.set_mode((500, 400))
+pygame.display.set_caption("Boundary sprite")
+bg_color = BLUE
+screen.fill(bg_color)
+
+
+exit = False
+clock = pygame.time.Clock()
+
+while not exit:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            exit = True
+        elif event.type == Sprite_color_change_event:
+            sp1.change_color()
+        elif event.type == Background_color_change_event:
+            change_background_color()
+    all_sprites_list.update()
+    screen.fill(bg_color)
+    all_sprites_list.draw(screen)
+
+    pygame.display.flip()
+    clock.tick(240)
+pygame.quit()
